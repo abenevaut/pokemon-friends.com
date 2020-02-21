342cf3de2fa82e9e1519d79487f082a155e953ce
Antoine Benevaut <a.benevaut@creads.org>
2020-02-21 13:59:23 +0100

    start to setup pokemon-friends.com

--- app/Http/Controllers/Customer/Users/UsersController.php
+++ app/Http/Controllers/Customer/Users/UsersController.php
@@ -2,25 +2,28 @@
 
 namespace template\Http\Controllers\Customer\Users;
 
+use Illuminate\Http\Request;
+use template\Domain\Users\Profiles\Repositories\ProfilesRepositoryEloquent;
+use template\Domain\Users\Users\User;
+use template\Http\Request\Administrator\Users\Profiles\ProfileFormRequest;
 use template\Infrastructure\Contracts\Controllers\ControllerAbstract;
-use template\Domain\Users\Users\Repositories\UsersRepositoryEloquent;
 
 class UsersController extends ControllerAbstract
 {
 
     /**
-     * @var null
+     * @var null|ProfilesRepositoryEloquent
      */
-    protected $r_users = null;
+    protected $r_profiles = null;
 
     /**
      * UsersController constructor.
      *
-     * @param UsersRepositoryEloquent $r_users
+     * @param ProfilesRepositoryEloquent $r_profiles
      */
-    public function __construct(UsersRepositoryEloquent $r_users)
+    public function __construct(ProfilesRepositoryEloquent $r_profiles)
     {
-        $this->r_users = $r_users;
+        $this->r_profiles = $r_profiles;
     }
 
     /**
@@ -35,5 +38,66 @@ class UsersController extends ControllerAbstract
         }
 
         return abort('404');
+    }
+
+    /**
+     * @param User $user
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Exception
+     */
+    public function edit(User $user)
+    {
+        try {
+            return view(
+                'administrator.users.profiles.profile',
+                [
+                    'profile' => $this->r_profiles->getUserProfile($user),
+                    'families_situations' => $this
+                        ->r_profiles
+                        ->getFamilySituations()
+                        ->mapWithKeys(function($item) {
+                            return [$item => trans("users.profiles.family_situation.{$item}")];
+                        }),
+                    'timezones' => $this->r_profiles->getTimezones(),
+                    'locales' => $this->r_profiles->getLocales(),
+                    'civilities' => $this->r_profiles->getCivilities(),
+                ]
+            );
+        } catch (\Exception $exception) {
+            app('sentry')->captureException($exception);
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * @param User $user
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Exception
+     */
+    public function profile(Request $request)
+    {
+        return $this->edit($request->user());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param integer $id Profile id
+     * @param ProfileFormRequest $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update($id, ProfileFormRequest $request)
+    {
+        try {
+            $this->r_profiles->updateUserProfileWithRequest($request, $id);
+        } catch (\Prettus\Validator\Exceptions\ValidatorException $exception) {
+            app('sentry')->captureException($exception);
+        }
+
+        return redirect(route('backend.users.profile.index'));
     }
 }
