a41f914e5757bba2951707df923d67ac4999da40
Antoine Benevaut <antoine@benevaut.fr>
2020-02-29 23:10:51 +0100

    Setup version in administrator footer

--- resources/lang/fr/global.php
+++ resources/lang/fr/global.php
@@ -5,6 +5,7 @@ return [
     'baseline' => 'Les rencontres pour réaliser vos fantasmes!',
     'changelog' => 'Changelog',
     'copyright' => '&copy; :date <a href=":route">:name</a>. All rights reserved.',
+    'version' => '<b>Version</b> :version',
     'terms' => 'Conditions générales d\'utilisation',
     'social_networks_baseline' => 'Suivez nous sur les réseaux sociaux',
 
