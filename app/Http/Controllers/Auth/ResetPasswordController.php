9ba734ca65e681cff0011bac69d2ea6e1e9328ff
Antoine Benevaut <antoine@benevaut.fr>
2020-03-23 09:11:20 +0100

    do not  recaptcha the reset password page

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,9 +2,7 @@
 
 namespace template\Http\Controllers\Auth;
 
-use GuzzleHttp\Client as GuzzleHttpClient;
 use Illuminate\Foundation\Auth\ResetsPasswords;
-use Illuminate\Http\Request;
 use template\Domain\Users\Users\Repositories\UsersResetPasswordRepositoryEloquent;
 use template\Infrastructure\Contracts\Controllers\ControllerAbstract;
 
@@ -30,11 +28,6 @@ class ResetPasswordController extends ControllerAbstract
     protected $r_users;
 
     /**
-     * @var Request
-     */
-    protected $request;
-
-    /**
      * Create a new controller instance.
      *
      * @return void
@@ -46,49 +39,12 @@ class ResetPasswordController extends ControllerAbstract
     }
 
     /**
-     * Reset the given user's password.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
-     */
-    public function reset(Request $request)
-    {
-        $this->request = $request;
-
-        return parent::reset($request);
-    }
-
-    /**
      * Get the password reset validation rules.
      *
      * @return array
      */
     protected function rules()
     {
-        $rules = $this->r_users->getResetPasswordRules();
-        $rules['g-recaptcha-response'] = 'required';
-
-        if (
-            app()->environment('local')
-            || app()->environment('testing')
-        ) {
-            unset($rules['g-recaptcha-response']);
-        } else {
-            $remoteUrl = sprintf(
-                'https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s&remoteip=%s',
-                config('services.google_recaptcha.serverkey'),
-                $this->request->get('g-recaptcha-response'),
-                $_SERVER['REMOTE_ADDR']
-            );
-
-            $response = (new GuzzleHttpClient())
-                ->request('GET', $remoteUrl);
-
-            if (200 !== $response->getStatusCode()) {
-                abort(403, 'Recaptcha verification failed!');
-            }
-        }
-
-        return $rules;
+        return $this->r_users->getResetPasswordRules();
     }
 }
