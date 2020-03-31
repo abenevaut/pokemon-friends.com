dc0a772ec7c2aa9a505e572d3d6b2313a067467b
Antoine Benevaut <antoine@benevaut.fr>
2020-04-01 00:54:05 +0200

    test sentry dialog component

--- tests/Unit/Vue/SentryDialogComponent.spec.js
+++ tests/Unit/Vue/SentryDialogComponent.spec.js
@@ -3,7 +3,6 @@ import { shallowMount } from '@vue/test-utils';
 import SentryDialogComponent from '@/components/SentryDialogComponent.vue';
 
 const sinon = require('sinon');
-const sentry = require('@sentry/browser');
 const apiMocks = require('./api-mocks');
 
 describe('SentryDialogComponent.vue', () => {
@@ -12,16 +11,15 @@ describe('SentryDialogComponent.vue', () => {
     apiMocks.beforeEach();
   });
 
-  it('show sentry dialog', async () => {
+  it('watch user variable', async () => {
     apiMocks.getUser();
+
+    const user = sinon.stub();
     const wrapper = shallowMount(SentryDialogComponent, {
-      props: { eventId: 'eventIdTest' },
+      watch: { user },
     });
 
-    var sentryMock = sinon.mock(sentry);
-
     await wrapper.vm.$nextTick();
-    // sentryMock.expects('showReportDialog').once();
-    sentryMock.verify();
+    expect(user.called).toBeTruthy();
   });
 });
