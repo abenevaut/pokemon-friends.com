f6549bc69076d07459f2efba781e36a91b68a0d0
Antoine Benevaut <antoine@benevaut.fr>
2020-04-16 01:32:03 +0200

    add test

--- tests/Feature/Http/Controllers/Api/V1/Users/UsersControllerTest.php
+++ tests/Feature/Http/Controllers/Api/V1/Users/UsersControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace template\Http\Controllers\Api\V1\Users;
 
+use Carbon\Carbon;
 use Tests\TestCase;
 use Tests\OAuthTestCaseTrait;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -22,7 +23,6 @@ class UsersControllerTest extends TestCase
         $user = factory(User::class)->create();
         factory(Profile::class)->create(['user_id' => $user->id]);
         Passport::actingAs($user);
-
         $this
             ->getJson('/api/v1/users/user')
             ->assertSuccessful()
@@ -42,9 +42,48 @@ class UsersControllerTest extends TestCase
         $user = factory(User::class)->create();
         factory(Profile::class)->create(['user_id' => $user->id]);
         Passport::actingAs($user);
-
         $this
-            ->getJson("/api/v1/users/trainer/{$user->uniqid}")
-            ->assertSuccessful();
+            ->get("/api/v1/users/qr/{$user->uniqid}.gif")
+            ->assertSuccessful()
+            ->assertHeader('Content-Type', 'image/gif');
+    }
+
+    public function testToGetUserQrCodeAsAnonymous()
+    {
+        $user = factory(User::class)->create();
+        factory(Profile::class)->create(['user_id' => $user->id]);
+        $this
+            ->get("/api/v1/users/qr/{$user->uniqid}.gif")
+            ->assertSuccessful()
+            ->assertHeader('Content-Type', 'image/gif');
+    }
+
+    public function testToGetUserQrCodeWhenUserDoesNotExist()
+    {
+        $this
+            ->get('/api/v1/users/qr/DOESNOTEXIST.gif')
+            ->assertNotFound();
+    }
+
+    public function testToGetUserQrCodeWhenUserNotSponsored()
+    {
+        $user = factory(User::class)->states(User::ROLE_CUSTOMER)->create();
+        factory(Profile::class)->create(['user_id' => $user->id, 'friend_code' => null]);
+        $this
+            ->get("/api/v1/users/qr/{$user->uniqid}.gif")
+            ->assertNotFound();
+    }
+
+    public function testToGetUserQrCodeWhenUserDeleted()
+    {
+        $user = factory(User::class)
+            ->states(User::ROLE_CUSTOMER)
+            ->create([
+                'deleted_at' => Carbon::now()->format('Y-m-d h:i:s'),
+            ]);
+        factory(Profile::class)->create(['user_id' => $user->id]);
+        $this
+            ->get("/api/v1/users/qr/{$user->uniqid}.gif")
+            ->assertNotFound();
     }
 }
