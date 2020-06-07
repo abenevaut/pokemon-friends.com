ccdb31194e9f86fa268d946000321c2904da00ef
Antoine Benevaut <antoine@benevaut.fr>
2020-06-07 09:13:15 +0200

    add gameforest dependencies

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -54,14 +54,6 @@ const files = require.context('./components/', true, /\.vue$/i);
 files.keys().map((key) => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 /**
- * laravel/passport components.
- */
-
-Vue.component('passport-clients', require('./components/passport/Clients.vue').default);
-Vue.component('passport-authorized-clients', require('./components/passport/AuthorizedClients.vue').default);
-Vue.component('passport-personal-access-tokens', require('./components/passport/PersonalAccessTokens.vue').default);
-
-/**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
