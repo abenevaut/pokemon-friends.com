bf78812a62a435d4eebf4644fcce45a631ef6ee4
Antoine Benevaut <a.benevaut@creads.org>
2020-08-14 17:13:33 +0200

    feat(app): dispatch friends codes on live streams

---
+++ app/App/Events/DispatchFriendCodeOnStreamEvent.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace pkmnfriends\App\Events;
+
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class DispatchFriendCodeOnStreamEvent implements ShouldBroadcast
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    public $streamChannel;
+    public $friendCode;
+
+    /**
+     * The name of the queue on which to place the event.
+     *
+     * @var string
+     */
+    public $broadcastQueue = 'high';
+
+    public function __construct($streamChannel, $friendCode)
+    {
+        $this->streamChannel = $streamChannel;
+        $this->friendCode = $friendCode;
+    }
+
+    public function broadcastOn()
+    {
+        return new PrivateChannel("stream.{$this->streamChannel}");
+    }
+
+    public function broadcastAs()
+    {
+        return 'add-qrcode';
+    }
+
+    /**
+     * Get the data to broadcast.
+     *
+     * @return array
+     */
+    public function broadcastWith()
+    {
+        return [
+            'streamChannel' => $this->streamChannel,
+            'friendCode' => $this->friendCode,
+        ];
+    }
+}
